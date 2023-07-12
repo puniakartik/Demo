ba5dea1e93a9fa71e0ea8b28017e868ac7b42611
Kartik Punia <kpunia@berkeley.edu>
2023-07-12 12:30:51 -0700

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Kartik Punia on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
